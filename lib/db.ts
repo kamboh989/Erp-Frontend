8a4d9175c5b2ac0522c264b2e4821944d16c092e
anasveer <anasaslam461@gmail.com>
2026-02-09 12:58:58 +0500

    Fix Vercel build crash (safe DB init)

--- lib/db.ts
+++ lib/db.ts
@@ -1,17 +1,26 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI!;
-if (!MONGODB_URI) throw new Error("Missing MONGODB_URI");
-
 declare global {
   // eslint-disable-next-line no-var
-  var mongooseCache: { conn: typeof mongoose | null; promise: Promise<typeof mongoose> | null };
+  var mongooseCache:
+    | { conn: typeof mongoose | null; promise: Promise<typeof mongoose> | null }
+    | undefined;
 }
 
-global.mongooseCache = global.mongooseCache || { conn: null, promise: null };
-
 export async function connectDB() {
-  if (global.mongooseCache.conn) return global.mongooseCache.conn;
+  const MONGODB_URI = process.env.MONGODB_URI;
+
+  if (!MONGODB_URI) {
+    throw new Error("MONGODB_URI is missing (runtime)");
+  }
+
+  if (!global.mongooseCache) {
+    global.mongooseCache = { conn: null, promise: null };
+  }
+
+  if (global.mongooseCache.conn) {
+    return global.mongooseCache.conn;
+  }
 
   if (!global.mongooseCache.promise) {
     global.mongooseCache.promise = mongoose.connect(MONGODB_URI, {
